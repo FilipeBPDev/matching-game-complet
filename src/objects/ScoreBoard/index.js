776b35581175fc81e3402ee75542955dcda01d9b
unknown <you@example.com>
2023-06-29 12:23:35 -0300

    :construction: Create HTML of the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,15 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/PlayerScore";
 import VsPlayer from "../../components/VsPlayer";
 import './style.css';
+
 function ScoreBoard() {
     return /*HTML*/`
         <header class = "score-board" >
         ${PlayerName ("Player 1")}
+        ${PlayerScore(2)}
         ${VsPlayer()}
+        ${PlayerScore(1)}
         ${PlayerName("Player 2")}
         </header> 
     `;
