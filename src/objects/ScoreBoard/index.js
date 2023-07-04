138a5c8d7af7e8f2b25e4dec5fdd6e035212d843
unknown <you@example.com>
2023-07-03 22:06:35 -0300

    :lipstick: Add Component ArrowDown and CSS with animation

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,3 +1,4 @@
+import ArrowDown from "../../components/ArrowDown";
 import PlayerName from "../../components/PlayerName";
 import PlayerScore from "../../components/PlayerScore";
 import VsPlayer from "../../components/VsPlayer";
@@ -6,11 +7,12 @@ import './style.css';
 function ScoreBoard() {
     return /*HTML*/`
         <header class = "score-board" >
-        ${PlayerName ("Player 1")}
-        ${PlayerScore(3)} <!--Player 1-->
-        ${VsPlayer()}
-        ${PlayerScore(1)} <!--Player 2-->
-        ${PlayerName("Player 2")}
+            ${ArrowDown(2)}
+            ${PlayerName ("Player 1")}
+            ${PlayerScore(3)} <!--Player 1-->
+            ${VsPlayer()}
+            ${PlayerScore(1)} <!--Player 2-->
+            ${PlayerName("Player 2")}
         </header> 
     `;
 }
