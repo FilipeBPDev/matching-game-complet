20067c60cbf7046ee40b35c9b01bb695a8ad8b4c
unknown <you@example.com>
2023-07-12 18:00:06 -0300

    :sparkles: Add click events to respective variables.

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,7 +2,7 @@ import './style.css'
 
 function CardGame(icon = "alura-pixel", alt = "Logo da Alura") {
     return /*html*/`
-    <article class = "card-game ${icon}" >
+    <article class = "card-game ${icon}" data-${icon} = "${icon}">
         <img src = "images/${icon}.png" alt = "${alt}">
     </article>
     `;
--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,12 +1,20 @@
 import './style.css';
 import CardFrontBack from '../../components/CardFrontBack';
+
 import cards from './data-cards';
 
 
 function BoardGame(amountCards) {
+   let firstCard = null;
+   let secondCard = null;
+
+
+
    const flipAndHideCards = ($cardsActive) => {
       $cardsActive.forEach((card) => card.classList.remove('-active'));
    }
+
+
    const swapPlayers = () => {
       const $arrowDown = document.querySelector('.arrow-down');
       const currentPlayer = parseInt($arrowDown.getAttribute('data-currentplayer'));
@@ -17,23 +25,53 @@ function BoardGame(amountCards) {
    window.boardGame = {};
    window.boardGame.handleClick = () => {
       const $boardGame = document.querySelector('.board-game');
-      
       const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active');
 
+      const cardsSelected = Array.from($cardsActive);
+      const card1Div = cardsSelected[0].querySelector('div:nth-child(2)');
+      const iconArticle = card1Div.querySelector('article');
+
+
+      if (cardsSelected.length > 1) {
+         const card2Div = cardsSelected[1].querySelector('div:nth-child(2)');
+         const iconArticle2 = card2Div.querySelector('article');
+
+         const firstSelected = iconArticle.getAttributeNames();
+         const secondSelected = iconArticle2.getAttributeNames();
+
+
+         firstCard = firstSelected[1].length;
+         secondCard = secondSelected[1].length;
+
+      }
+
       if ($cardsActive.length === 2) {
+         const checkCards = () => {
+            if (firstCard === secondCard){
+               $cardsActive[0].classList.add('-active');
+            }
+         };
+         console.log(firstCard, secondCard);
+         checkCards($cardsActive);
+
          setTimeout(() => {
-            
-            flipAndHideCards($cardsActive)
-            swapPlayers()
+
+            flipAndHideCards($cardsActive);
+            swapPlayers();
          }, 700);
       }
 
-   }
 
-    const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
-    const $htmlCards = htmlCardsList.join('');
 
-    return /*html*/`
+
+   };
+
+
+
+   const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
+   const $htmlCards = htmlCardsList.join('');
+
+   return /*html*/`
     <section class = "board-game" onClick = "boardGame.handleClick()">
        ${$htmlCards}
     </section>
